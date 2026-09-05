a1e011cded9a01bc43395a739c841fd42ea76d1d
Ethan <evaneyden@outlook.com>
2026-09-05 20:51:06 +0200

    -Added issue and composite interface

--- Component.h
+++ Component.h
@@ -8,34 +8,124 @@
 class State;
 template <typename T> class ProjectIterator;
 
+/**
+ * @brief This class acts at the Component in the composite hierachy.
+ *
+ */
 class Component {
 public:
   // Composite
+
+  /**
+   * @brief Add a component to the current one as child
+   *
+   * @param component
+   */
   virtual void add(Component *component);
+  /**
+   * @brief Remove the component from the current one
+   *
+   * @param component
+   */
   virtual void remove(Component *component);
+  /**
+   * @brief Get the Child object
+   *
+   * @param index
+   * @return Component*
+   */
   virtual Component *getChild(int index) const;
+  /**
+   * @brief Get the number of children
+   *
+   * @return int
+   */
   virtual int getChildCount() const;
 
   // State
+
+  /**
+   * @brief Get the name of the current state
+   *
+   * @return std::string
+   */
   virtual std::string getState() const = 0;
-  virtual void setState(const std::string& newState) = 0;
+  /**
+   * @brief Set the State
+   *
+   * @param newState
+   */
+  virtual void setState(const std::string &newState) = 0;
 
   // Core identity
+
+  /**
+   * @brief Get the name
+   *
+   * @return std::string
+   */
   virtual std::string getName() const;
+  /**
+   * @brief Get the ID
+   *
+   * @return std::string
+   */
   virtual std::string getID() const;
+  /**
+   * @brief Get the description
+   *
+   * @return std::string
+   */
   virtual std::string getDescription() const;
 
+  /**
+   * @brief Create an Iterator
+   *
+   * @return std::unique_ptr<ProjectIterator<Component>>
+   */
   virtual std::unique_ptr<ProjectIterator<Component>> createIterator() const;
 
+  /**
+   * @brief Construct a new Component object
+   *
+   * @param name
+   * @param id
+   * @param desc
+   */
   Component(const std::string &name, const std::string &id,
             const std::string &desc = "");
   virtual ~Component() = default;
 
-
+  /**
+   * @brief Checks if the object is in open state
+   *
+   * @return true
+   * @return false
+   */
   bool isOpen() const;
+  /**
+   * @brief Checks if object is in progress
+   *
+   * @return true
+   * @return false
+   */
   bool isInProgress() const;
+  /**
+   * @brief Checks if the object is closed
+   *
+   * @return true
+   * @return false
+   */
   bool isClosed() const;
-  virtual bool hasPriority(const std::string& level) const;
+
+  /**
+   * @brief Checks for the presence of the priority decorator
+   *
+   * @param level
+   * @return true
+   * @return false
+   */
+  virtual bool hasPriority(const std::string &level) const;
 
 protected:
   // Protected accessor for derived classes to manipulate the state pointer
---
+++ Composite.h
@@ -0,0 +1,31 @@
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
+
+#include "Component.h"
+#include <vector>
+
+/**
+ * @brief This class is the composite in the hierachy. It is non owning.
+ * 
+ */
+class Composite : Component {
+protected:
+  std::vector<Component *> children;
+
+public:
+  Composite(const std::string &name, const std::string &id,
+            const std::string &desc = "");
+  virtual ~Composite() = default;
+
+  std::string getState() const override;
+  void setState(const std::string &newState) override;
+
+  void add(Component *child) override;
+  void remove(Component *child) override;
+  Component *getChild(int index) const override;
+  int getChildCount() const override;
+
+  std::unique_ptr<ProjectIterator<Component>> createIterator() const override;
+};
+
+#endif
