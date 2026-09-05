3111016acad3babf59a08c3a83216a8113a9ced7
Ethan Van Eyden <evaneyden@outlook.com>
2026-09-05 16:25:55 +0200

    -Created component.h

---
+++ Component.h
@@ -0,0 +1,51 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class State;
+template <typename T> class ProjectIterator;
+
+class Component {
+public:
+  // Composite
+  virtual void add(Component *component);
+  virtual void remove(Component *component);
+  virtual Component *getChild(int index) const;
+  virtual int getChildCount() const;
+
+  // State
+  virtual std::string getState() const;
+  virtual void setState(State *state);
+
+  // Core identity
+  virtual std::string getName() const;
+  virtual std::string getID() const;
+  virtual std::string getDescription() const;
+
+  virtual std::unique_ptr<ProjectIterator<Component>> createIterator() const;
+
+  Component(const std::string &name, const std::string &id,
+            const std::string &desc = "");
+  virtual ~Component() = default;
+
+
+  bool isOpen() const;
+  bool isInProgress();
+  bool isClosed() const;
+
+protected:
+  // Protected accessor for derived classes to manipulate the state pointer
+  State *getStateObject() const;
+  void setStateObject(std::unique_ptr<State> newState);
+
+private:
+  std::unique_ptr<State> state;
+  std::string name;
+  std::string id;
+  std::string description;
+};
+
+#endif
